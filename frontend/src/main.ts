1c1b19a3af92effeb17f469029c204c4ef73e82b
ansis@home <ansis@home>
2024-01-29 19:41:22 +0200

    Service worker actually in action. Build with "ng build --configuration=development" and the serve "http-server ./dist/frontend".

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from './environments/environment';
-
 import { AppModule } from './app/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/ngsw-worker.js')
+        .then(registration => console.log('SW registered: ', registration))
+        .catch(registrationError => console.log('SW registration failed: ', registrationError));
+    }
+  })
   .catch(err => console.error(err));
-
-if ('serviceWorker' in navigator && environment.production) {
-  navigator.serviceWorker.register('/ngsw-worker.js');
-}
